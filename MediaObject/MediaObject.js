ca388a1aa738d4cf8504ac586cf5610a288ed0f1
Timothy Anthony <anthony.timothy90@gmail.com>
2019-05-07 13:21:56 +0300

    style(MediaObject) Using Tab Size 4

--- MediaObject/MediaObject.js
+++ MediaObject/MediaObject.js
@@ -5,42 +5,42 @@ import {ldswcconfig} from '../ldswcconfig.js';
 export default class MediaObject extends LitElement {
 	static get properties() {
 		return {
-      /**
+			/**
 			 * Optional class name which is appended to the body div
 			 */
-      bodyClassName: { type: String },
+			bodyClassName: { type: String },
 			/**
 			 * class name
 			 */
-      className: { type: String },
-      /**
+			className: { type: String },
+			/**
 			 * horizontally centers figures with content
 			 */
-      center: { type: Boolean },
-      /**
+			center: { type: Boolean },
+			/**
 			 * Renders a customTag instead of a div
 			 */
-      customTag: { type: String },
-      /**
+			customTag: { type: String },
+			/**
 			 * sets a figure on the left side of the media object
 			 */
-      figureLeft: { type: String },
-      /**
+			figureLeft: { type: String },
+			/**
 			 * sets a figure on the right side of the media object
 			 */
-      figureRight: { type: String },
-      /**
-       * Function to render figureLeft. Receives (elem, classes) as arguments
-       */
-      renderFigureLeft: { type: String },
-      /**
-       * Function to render figureRight. Receives (elem, classes) as arguments
-       */
-      renderFigureRight: { type: String },
-      /**
+			figureRight: { type: String },
+			/**
+			 * Function to render figureLeft. Receives (elem, classes) as arguments
+			 */
+			renderFigureLeft: { type: String },
+			/**
+			 * Function to render figureRight. Receives (elem, classes) as arguments
+			 */
+			renderFigureRight: { type: String },
+			/**
 			 * renders a responsive variant of the MediaObject
 			 */
-      responsive: { type: Boolean },
+			responsive: { type: Boolean },
 			/**
 			 * Sizes: small, large
 			 */
@@ -57,55 +57,55 @@ export default class MediaObject extends LitElement {
 	}
 
 	constructor() {
-    super();
-    this.bodyClassName = null;
-    this.className = null;
-    this.center = true;
-    this.customTag = null;
-    this.figureLeft = null;
-    this.figureRight = null;
-    this.responsive = null;
-    this.size = null;
-    this.truncate = null;
-    this.title = null;
-    this.renderFigureLeft = null;
-    this.renderFigureRight = null;
+		super();
+		this.bodyClassName = null;
+		this.className = null;
+		this.center = true;
+		this.customTag = null;
+		this.figureLeft = null;
+		this.figureRight = null;
+		this.responsive = null;
+		this.size = null;
+		this.truncate = null;
+		this.title = null;
+		this.renderFigureLeft = null;
+		this.renderFigureRight = null;
   }
-
-  renderFigure(figure, classes) {
-    return html`
-  <div class=${joinClassNames(classes)}>${figure}</div>`
-  }
+  
+ 	renderFigure(figure, classes) {
+		return html`
+<div class=${joinClassNames(classes)}>${figure}</div>`
+	}
 
 	render() {
 		//const otherattrs = getRestOfAttribs(this.attributes, this.constructor.properties);
-    const Tag = this.customTag || 'div';
-
-    const figureLeftRenderer = this.renderFigureLeft || this.renderFigure;
-    const figureRightRenderer = this.renderFigureRight || this.renderFigure;
-
+		const Tag = this.customTag || 'div';
+		
+		const figureLeftRenderer = this.renderFigureLeft || this.renderFigure;
+		const figureRightRenderer = this.renderFigureRight || this.renderFigure;
+		
 		const sldsClasses = [
-      'slds-media',
-      { 'slds-media_center': this.center },
-      { 'slds-media_responsive': this.responsive },
-      { [`slds_media_${this.size}`]: !!this.size},
-      this.className
-    ];
-    
-    const bodyClasses = [
-      'slds-media__body',
-      { 'slds--truncate': this.truncate },
-      this.bodyClassName
-    ];
-
-    const leftFigureClasses = [
-      'slds-media__figure'
-    ];
-
-    const rightFigureClasses = [
-      'slds-media__figure',
-      'slds-media__figure_reverse'
-    ];
+			'slds-media',
+			{ 'slds-media_center': this.center },
+			{ 'slds-media_responsive': this.responsive },
+			{ [`slds_media_${this.size}`]: !!this.size},
+			this.className
+		];
+		
+		const bodyClasses = [
+			'slds-media__body',
+			{ 'slds--truncate': this.truncate },
+			this.bodyClassName
+		];
+		
+		const leftFigureClasses = [
+			'slds-media__figure'
+		];
+		
+		const rightFigureClasses = [
+			'slds-media__figure',
+			'slds-media__figure_reverse'
+		];
 
 		return html`
 <style>
